0f066e6b9bf51cb228cf013b674e0404d7f0f617
unknown <natorvano.work@gmail.com>
2018-11-16 00:35:21 +0200

    declare func variables in index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -17,17 +17,23 @@ export interface ConnectionInfo {
 }
 
 export type Connect = (uri: string, dbName: string) => Promise<Connection>;
+export const connect: Connect;
 
 export type Disconnect = (client: MongoClient) => Promise<void>;
+export const disconnect: Disconnect;
 
 export type RunQuery = <T>(
   uri: string,
   dbName: string,
   query: Query<T>
 ) => Promise<T>;
+export const runQuery: RunQuery;
 
 export type CloseTestDb = (connection: Connection) => Promise<void>;
+export const clostTestDb: CloseTestDb;
 
 export type PrepareTestDb = (dbName: string) => Promise<Connection>;
+export const prepareTestDb: PrepareTestDb;
 
 export type GetConnectionInfo = (processEnv: ProcessEnv) => ConnectionInfo;
+export const getConnectionInfo: GetConnectionInfo;
